c02c03dd3bbfdecf6323f0d30bd8161df8841717
Oshani Kumarathunga <oshanipbkonline@gmail.com>
2023-07-19 02:27:03 +0530

    Initial commit

---
+++ src/main/java/com/springboot/todolist_application_springboot/TodoListApplicationSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.todolist_application_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListApplicationSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoListApplicationSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/todolist_application_springboot/TodoListApplicationSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.todolist_application_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListApplicationSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
